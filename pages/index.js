c6884354fb022c9333780d1dfaab337346fe5c20
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-01-26 00:12:23 -0300

    Create QuizContainer component file

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,11 @@
-import styled from 'styled-components'
-
 import configs from '../configs.json'
 
 import BackgroundImage from '../src/components/BackgroundImage'
 import Footer from '../src/components/Footer'
 import GithubCorner from '../src/components/GithubCorner'
+import QuizContainer from '../src/components/QuizContainer'
 import QuizLogo from '../src/components/QuizLogo'
 import Widget from '../src/components/Widget'
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`
 
 export default function Home() {
   return (
