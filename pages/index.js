a9b3a5e8d73c928158d839f2dc17e09db7f1c59c
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-01-26 00:10:48 -0300

    Add Logo

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import styled from 'styled-components'
 
 import configs from '../configs.json'
 
-import Widget from '../src/components/Widget'
+import BackgroundImage from '../src/components/BackgroundImage'
 import Footer from '../src/components/Footer'
 import GithubCorner from '../src/components/GithubCorner'
-import BackgroundImage from '../src/components/BackgroundImage'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
 
 const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
+  
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
@@ -22,6 +24,7 @@ export default function Home() {
   return (
     <BackgroundImage backgroundImage={configs.bg}>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{configs.title}</h1>
