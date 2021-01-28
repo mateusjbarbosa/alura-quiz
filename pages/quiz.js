4ca2f65431fee5ab0524fc1b8d4b536a98ff9424
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-01-28 09:53:08 -0300

    Create quiz question page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,151 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function QuizPage() {
+import configs from '../configs.json';
+
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import BackgroundImage from '../src/components/BackgroundImage';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
+
+function LoadingWidget() {
   return (
-    <h1>Quiz</h1>
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
   );
 }
 
-export default QuizPage;
+function QuestionWidget({
+  question,
+  questionIndex,
+  totalQuestions,
+  onSubmit,
+}) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit();
+          }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
+export default function QuizPage() {
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const totalQuestions = configs.questions.length;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questionIndex = currentQuestion;
+  const question = configs.questions[questionIndex];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <BackgroundImage backgroundImage={configs.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+      </QuizContainer>
+    </BackgroundImage>
+  );
+}
+
+QuestionWidget.propTypes = {
+  question: PropTypes.exact({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    answer: PropTypes.number,
+    alternatives: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+  questionIndex: PropTypes.number.isRequired,
+  totalQuestions: PropTypes.number.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
