98c283ee98620bf43225cb932345cc1aa638db39
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-01-29 23:42:20 -0300

    Get playerName param with getServerSideProps

--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
 
 import QuizScreen from '../../src/screens/Quiz';
 
 import db from '../../db.json';
 
-export default function QuizPage() {
+export default function QuizPage({ pageProps }) {
+  const { playerName } = pageProps;
   return (
     <ThemeProvider theme={db.theme}>
+      <h1>{playerName}</h1>
       <QuizScreen
         externalQuestions={db.questions}
         externalBg={db.bg}
+        playerName={playerName}
       />
     </ThemeProvider>
   );
 }
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      playerName: context.query.playerName,
+    },
+  };
+}
+
+QuizPage.propTypes = {
+  pageProps: PropTypes.exact({
+    playerName: PropTypes.string,
+  }).isRequired,
+};
